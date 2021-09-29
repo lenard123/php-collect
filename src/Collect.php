15a9d6c77e59d54b7b2088fff2ff7cae85e5f21e
Lenard Mangay-ayam <lenard.mangayayam@gmail.com>
2021-09-29 22:07:35 +0800

    Initialize Repository

---
+++ src/Collect.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Lenard123\PHPCollect;
+
+class Collect
+{
+
+
+  private array $data;
+
+
+
+
+  public function __construct(array $args)
+  {
+    $this->data = $args;
+  }
+
+
+
+
+  /**
+   * Return all the data as array
+   **/
+  public function all() : array
+  {
+    return $this->data;
+  }
+
+
+
+
+
+  /**
+   * Returns the average of the collection
+   */
+  public function avg($key = null)
+  {
+    if (is_null($key)) {
+    }
+  }
+
+
+
+
+  public function reduce(callable $callback, $initial = null)
+  {
+    return array_reduce($this->data, $callback, $initial);
+  }
+
+
+
+  public function map(callable $callback) : Collect
+  {
+    $result = array_map($callback, $this->data);
+    return self::create($result);
+  }
+
+
+
+
+  /**
+   * Return the sum of the data
+   */
+  public function sum($key = null)
+  {
+    if (is_null($key)) return array_sum($this->data);
+
+    $total = $this->reduce(function($acm, $item) use ($key) { 
+      $value = $item[$key] ?? 0;
+      return $acm + $value;
+    }, 0);
+
+    return $total;
+  }
+
+
+
+
+
+  //-- STATIC METHODS   
+  public static function create(array $data)
+  {
+    return new self($data);
+  }
+}
